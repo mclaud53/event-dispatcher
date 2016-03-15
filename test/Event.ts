6d449a1da863ff3a05bf3fe003bb633869e4361b
George Matvieiev <varfolomey@gmail.com>
2016-03-16 01:07:35 +0200

    Added tests for class Event

--- test/Event.ts
+++ test/Event.ts
@@ -4,5 +4,15 @@ import assert = require('assert');
 require('sinomocha')();
 
 describe('Event', function () {
-	
+	it('creation', function() {
+		var type: string = 'TEST',
+			target: Object = {},
+			options: Object = {key: 'value'},
+			instance: event.Event<Object> = new event.Event<Object>(type, target, true, { key: 'value' });
+
+		assert.equal(instance.type, type, 'The wrong type of event');
+		assert.equal(instance.target, target, 'The wrong target of event');
+		assert.equal(instance.cancellable, true, 'The event isn\'t cancellable');
+		assert.deepEqual(instance.options, options, 'The wrong options of event');
+	});
 });
